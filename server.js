b3fbaef5b06bb146bfe9b39eafb9087418a3d646
Mark Honeychurch <mark@honeychurch.org>
2019-08-20 13:00:38 +1200

    About to remove duplicate code

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Papa = require('papaparse');
 const path = require('path');
 const fs = require('fs');
@@ -33,7 +34,8 @@ function cleanBook(book) {
 	book.Author = book.Author.replace("Ackroyed", "Ackroyd");
 	book.Title = book.Title.replace("Nonsence", "Nonsense");
 	if (book.Author.includes(", ")) {
-		book.Author = book.Author.split(", ").reverse().join(" ");
+		//book.Author = book.Author.split(", ").reverse().join(" ");
+		book.Author = book.Author.split(";").map(author => author.split(", ").reverse().join(" ")).join(", ");
 	} else {
 		book.Author = book.Author.split(" ", 2).reverse().join(" ");
 	}
@@ -113,9 +115,9 @@ async function getGoogleBooks(book) {
 	} else {
 		if (download) {
 			try {
-				matches = await googlebooks.searchPromise(book.Title);
+				matches = await googlebooks.searchPromise(encodeURIComponent(book.Title), {key: process.env.GOOGLE_API_KEY});
 			} catch(error) {
-				//console.log(error);
+				console.log(error);
 			}
 			if (matches && matches.length) {
 				fs.writeFileSync(cachepath, JSON.stringify(matches, null, '\t'));
@@ -125,9 +127,24 @@ async function getGoogleBooks(book) {
 	return matches;
 }
 
+async function getGoodReads(book) {
+
+}
+
+function matchPercent(title1, title2, authors1, authors2) {
+	if (title1.includes(":") && levenshtein(title1.toLowerCase(), title2.toLowerCase()) > levenshtein(title1.split(":", 2)[0].toLowerCase(), title2.toLowerCase())) {
+		title1 = title1.split(":", 2)[0];
+	}
+	const length = title1.length + authors1.length;
+	const titlelev = levenshtein(title1.toLowerCase(), title2.toLowerCase());
+	const authorlev = (authors2 && authors2.length ? levenshtein(authors1.toLowerCase(), authors2.join(", ").toLowerCase()) : authors1.length);
+	const percent = (1 - ((titlelev + authorlev) / length)) * 100;
+	return percent;
+}
+
 function findBestMatch(book, matches) {
 	let bestmatch;
-	let lowest = 999;
+	let highest = 0;
 	for (let match of matches) {
 		if (book.ISBN && match.isbn && match.isbn.includes(book.ISBN)) {
 			return match;
@@ -135,40 +152,107 @@ function findBestMatch(book, matches) {
 		/*if (match.publisher && match.publish_year && match.publisher.includes(book.Publisher) && match.publish_year.includes(book.Date)) {
 			return match;
 		}*/
-		const lev = Math.min(levenshtein(book.Title.split(":")[0].toLowerCase(), match.title.toLowerCase()), levenshtein(book.Title.toLowerCase(), match.title.toLowerCase())) + (match.author_name && match.author_name.length ? levenshtein(book.Author.toLowerCase(), match.author_name.join(", ").toLowerCase()) : 5);
-		if (lev < lowest && lev <= 10) {
-			lowest = lev;
+		const percent = matchPercent(book.Title, match.title, book.Author, match.authors);
+		console.log(percent);
+		if (percent > highest && percent >= lowestPercent) {
+			highest = percent;
 			bestmatch = match;
 		}
+	}
+	if (bestmatch) return {...bestmatch, percent: highest};
+}
+
+function findBestOpenLibraryMatch(book, matches) {
+	let bestmatch;
+	let highest = 0;
+	for (let match of matches) {
+		if (book.ISBN && match.isbn && match.isbn.includes(book.ISBN)) {
+			return match;
+		}
+		/*if (match.publisher && match.publish_year && match.publisher.includes(book.Publisher) && match.publish_year.includes(book.Date)) {
+			return match;
+		}*/
+		//const lev = Math.min(levenshtein(book.Title.split(":")[0].toLowerCase(), match.title.toLowerCase()), levenshtein(book.Title.toLowerCase(), match.title.toLowerCase())) + (match.author_name && match.author_name.length ? levenshtein(book.Author.toLowerCase(), match.author_name.join(", ").toLowerCase()) : 5);
+		const percent = matchPercent(book.Title, match.title, book.Author, match.author_name);
+		console.log(percent);
+		if (percent > highest && percent >= lowestPercent) {
+			highest = percent;
+			bestmatch = match;
+		}
+	}
+	if (bestmatch) {
+		bestmatch.percent = highest;
+		bestmatch.authors = bestmatch.author_name;
 	}
 	return bestmatch;
 }
 
-function printResult(book, match) {
-	console.log(book.Title, levenshtein(book.Title.toLowerCase(), match.title.toLowerCase()), match.title);
-	if (match.author_name && match.author_name.length) console.log(book.Author, levenshtein(book.Author.toLowerCase(), match.author_name[0].toLowerCase()), match.author_name[0]);
+function findBestGoogleBooksMatch(book, matches) {
+	let bestmatch;
+	let highest = 0;
+	for (let match of matches) {
+		if (book.ISBN && match.industryIdentifiers && match.industryIdentifiers.map(id => id.identifier).includes(book.ISBN)) {
+			return match;
+		}
+		/*if (match.publisher && match.publish_year && match.publisher.includes(book.Publisher) && match.publish_year.includes(book.Date)) {
+			return match;
+		}*/
+		if (match.title) {
+			const percent = matchPercent(book.Title, match.title, book.Author, match.authors);
+			//const lev = Math.min(levenshtein(book.Title.split(":")[0].toLowerCase(), match.title.toLowerCase()), levenshtein(book.Title.toLowerCase(), match.title.toLowerCase())) + (match.authors && match.authors.length ? levenshtein(book.Author.toLowerCase(), match.authors.join(", ").toLowerCase()) : 5);
+			if (percent > highest && percent >= lowestPercent) {
+				lowest = percent;
+				bestmatch = match;
+			}
+			}
+	}
+	if (bestmatch) bestmatch.percent = highest;
+	return bestmatch;
 }
 
-const download = true;
+function printResult(book, match) {
+	console.log(book.Title, "=>", book.Author.split(", "), match.percent);
+	console.log(match.title, "=>", match.authors, match.percent);
+}
+
+const download = false;
+const lowestPercent = 90;
 
 async function all() {
 	let books = await getBooks();
 	let total = 0;
 	for (let book of books) {
 		book = cleanBook(book);
-		let matches = await getOpenLibrary(book);
-		if (!matches) {
-			matches = await getGoogleBooks(book);
-		}
-		/*if (matches && matches.length) {
-			const match = findBestMatch(book, matches);
+		let matches;
+		let match;
+		matches = await getGoogleBooks(book);
+		if (matches && matches.length) {
+			match = findBestMatch(book, matches.map(match => {
+				return {
+					...match,
+					isbn: (match.industryIdentifiers ? match.industryIdentifiers.map(id => id.identifier) : []),
+				};
+			}));
 			if (match) {
 				total++;
-				if (Math.min(levenshtein(book.Title.split(":")[0].toLowerCase(), match.title.toLowerCase()), levenshtein(book.Title.toLowerCase(), match.title.toLowerCase())) + (match.author_name && match.author_name.length ? levenshtein(book.Author.toLowerCase(), match.author_name.join(", ").toLowerCase()) : 5) > 10) {
-					printResult(book, match);
+			} else {
+				matches = await getOpenLibrary(book);
+				if (matches && matches.length) {
+					match = findBestMatch(book, matches.map(match => {
+						return {
+							...match,
+							authors: matches.author_name,
+						};
+					}));
+					if (match) {
+						total++;
+					}
 				}
 			}
-		}*/
+		}
+		if (match && (match.percent < 82)) {
+			//printResult(book, match);
+		}
 	}
 	console.log(total);
 }
