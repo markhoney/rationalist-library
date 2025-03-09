080fbf26d142a714de1b533613952c6a0cf72989
Mark Honeychurch <mark@honeychurch.org>
2025-03-09 23:20:13 +1300

    Adding lookups

--- process.js
+++ process.js
@@ -3,7 +3,10 @@ const Papa = require('papaparse');
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
+const isbn = require('node-isbn');
 const googlebooks = require('google-books-search');
+const {GoogleBooksAPI} = require("google-books-js");
+const googleBooksApi = new GoogleBooksAPI();
 const sanitize = require("sanitize-filename");
 const levenshtein = require('js-levenshtein');
 const util = require('util');
@@ -36,6 +39,8 @@ function cleanBook(book) {
 	if (book.Author.includes(", ")) {
 		//book.Author = book.Author.split(", ").reverse().join(" ");
 		book.Author = book.Author.split(";").map(author => author.split(", ").reverse().join(" ")).join(", ");
+	} else if (book.Author.includes(" & ")) {
+		book.Author = book.Author.split(" & ").join(", ");
 	} else {
 		book.Author = book.Author.split(" ", 2).reverse().join(" ");
 	}
@@ -67,14 +72,42 @@ function cleanBook(book) {
 			book.Currency = "NZD";
 		}
 	}
-	book.ISBN = book.ISBN.replace(/\-/g, "");
+	book.ISBN = book.ISBN.trim().replace(/\-/g, "");
 	return book;
 }
 
+async function getISBN(book) {
+	if (!book.ISBN) return [];
+	const cachefolder = path.join(__dirname, 'cache', 'ISBN');
+	if (!fs.existsSync(cachefolder)) fs.mkdirSync(cachefolder, {recursive: true});
+	const cachepath = path.join(cachefolder, sanitize(book.ISBN) + '.json');
+	let match = {};
+	if (fs.existsSync(cachepath)) match = require(cachepath);
+	else {
+		if (download) {
+			try {
+				match = await isbn.resolve(book.ISBN); // .provider(['google', 'openlibrary', 'worldcat'])
+				// await new Promise(resolve => setTimeout(resolve, 1000));
+			} catch(error) {
+				// console.log(error);
+			}
+			fs.writeFileSync(cachepath, JSON.stringify(match, null, '\t'));
+		}
+	}
+	if (match) return [{
+		...match,
+		isbn: match.industryIdentifiers?.length ? match.industryIdentifiers.map(id => id.identifier).concat([book.ISBN]) : [book.ISBN],
+		source: 'ISBN',
+	}];
+	return [];
+}
+
 async function getOpenLibrary(book) {
-	const cachepath = path.join(__dirname, 'cache', 'OpenLibrary', sanitize(book.Title + " - " + book.Author) + '.json');
-	const cachepath2 = path.join(__dirname, 'cache', 'OpenLibrary', sanitize(book.Title) + '.json');
-	let matches;
+	const cachefolder = path.join(__dirname, 'cache', 'OpenLibrary');
+	if (!fs.existsSync(cachefolder)) fs.mkdirSync(cachefolder, {recursive: true});
+	const cachepath = path.join(cachefolder, sanitize(book.Title + " - " + book.Author) + '.json');
+	const cachepath2 = path.join(cachefolder, sanitize(book.Title) + '.json');
+	let matches = [];
 	if (fs.existsSync(cachepath) || fs.existsSync(cachepath2)) {
 		if (fs.existsSync(cachepath)) {
 			matches = require(cachepath);
@@ -84,47 +117,83 @@ async function getOpenLibrary(book) {
 	} else {
 		if (download) {
 			try {
+				console.log('Downloading from OpenLibrary:', book.Title, book.Author);
 				const url = 'http://openlibrary.org/search.json?title=' + encodeURIComponent(book.Title) + '&author=' + encodeURIComponent(book.Author);
 				matches = (await axios.get(url)).data.docs;
+				// await new Promise(resolve => setTimeout(resolve, 1000));
 			} catch(error) {
-				//console.log(error);
+				// console.log(error);
 			}
 			if (matches && matches.length) {
 				fs.writeFileSync(cachepath, JSON.stringify(matches, null, '\t'));
 			} else {
 				try {
+					console.log('Downloading from OpenLibrary:', book.Title);
 					const url = 'http://openlibrary.org/search.json?title=' + encodeURIComponent(book.Title);
 					matches = (await axios.get(url)).data.docs;
 				} catch(error) {
-					//console.log(error);
+					// console.log(error);
+					return [];
 				}
-				if (matches && matches.length) {
-					fs.writeFileSync(cachepath2, JSON.stringify(matches, null, '\t'));
-				}
+				fs.writeFileSync(cachepath2, JSON.stringify(matches, null, '\t'));
 			}
 		}
 	}
-	return matches;
+	matches = matches?.map(match => ({
+		...match,
+		authors: match.author_name,
+		source: 'OpenLibrary',
+	}));
+	return matches || [];
 }
 
 async function getGoogleBooks(book) {
-	const cachepath = path.join(__dirname, 'cache', 'GoogleBooks', sanitize(book.Title) + '.json');
-	let matches;
-	if (fs.existsSync(cachepath)) {
-		matches = require(cachepath);
+	const cachefolder = path.join(__dirname, 'cache', 'GoogleBooks');
+	if (!fs.existsSync(cachefolder)) fs.mkdirSync(cachefolder, {recursive: true});
+	const cachepath = path.join(cachefolder, sanitize(book.Title + " - " + book.Author) + '.json');
+	const cachepath2 = path.join(cachefolder, sanitize(book.Title) + '.json');
+	let matches = [];
+	if (fs.existsSync(cachepath)) { // || fs.existsSync(cachepath2)
+		if (fs.existsSync(cachepath)) {
+			matches = require(cachepath);
+		} else {
+			matches = require(cachepath2);
+		}
 	} else {
 		if (download) {
 			try {
-				matches = await googlebooks.searchPromise(encodeURIComponent(book.Title), {key: process.env.GOOGLE_API_KEY});
+				console.log('Downloading from GoogleBooks:', book.Title, '-', book.Author);
+				const books = await googleBooksApi.search({
+					filters: {
+						title:  book.Title,
+						author: book.Author,
+					},
+				});
+				matches = books.items.map((book) => book.volumeInfo);
 			} catch(error) {
 				console.log(error);
 			}
 			if (matches && matches.length) {
 				fs.writeFileSync(cachepath, JSON.stringify(matches, null, '\t'));
+			} else {
+				try {
+					console.log('Downloading from GoogleBooks:', book.Title);
+					matches = await googlebooks.searchPromise(encodeURIComponent(book.Title), {key: process.env.GOOGLE_API_KEY});
+					// await new Promise(resolve => setTimeout(resolve, 1000));
+				} catch(error) {
+					console.log(error);
+					return [];
+				}
+				fs.writeFileSync(cachepath2, JSON.stringify(matches, null, '\t'));
 			}
 		}
 	}
-	return matches;
+	matches = matches?.map(match => ({
+		...match,
+		isbn: match.industryIdentifiers ? match.industryIdentifiers.map(id => id.identifier) : [],
+		source: 'GoogleBooks',
+	}));
+	return matches || [];
 }
 
 async function getGoodReads(book) {
@@ -132,6 +201,7 @@ async function getGoodReads(book) {
 }
 
 function matchPercent(title1, title2, authors1, authors2) {
+	// console.log([title1, authors1, title2, authors2].join(" | "));
 	if (title1.includes(":") && levenshtein(title1.toLowerCase(), title2.toLowerCase()) > levenshtein(title1.split(":", 2)[0].toLowerCase(), title2.toLowerCase())) {
 		title1 = title1.split(":", 2)[0];
 	}
@@ -146,68 +216,20 @@ function findBestMatch(book, matches) {
 	let bestmatch;
 	let highest = 0;
 	for (let match of matches) {
-		if (book.ISBN && match.isbn && match.isbn.includes(book.ISBN)) {
-			return match;
+		if (book.ISBN && match.isbn) {
+			if (match.isbn.includes(book.ISBN)) return {...match, percent: 101};
 		}
 		/*if (match.publisher && match.publish_year && match.publisher.includes(book.Publisher) && match.publish_year.includes(book.Date)) {
 			return match;
 		}*/
 		const percent = matchPercent(book.Title, match.title, book.Author, match.authors);
-		console.log(percent);
-		if (percent > highest && percent >= lowestPercent) {
+		// console.log(percent);
+		if (percent > highest) {
 			highest = percent;
 			bestmatch = match;
 		}
 	}
-	if (bestmatch) return {...bestmatch, percent: highest};
-}
-
-function findBestOpenLibraryMatch(book, matches) {
-	let bestmatch;
-	let highest = 0;
-	for (let match of matches) {
-		if (book.ISBN && match.isbn && match.isbn.includes(book.ISBN)) {
-			return match;
-		}
-		/*if (match.publisher && match.publish_year && match.publisher.includes(book.Publisher) && match.publish_year.includes(book.Date)) {
-			return match;
-		}*/
-		//const lev = Math.min(levenshtein(book.Title.split(":")[0].toLowerCase(), match.title.toLowerCase()), levenshtein(book.Title.toLowerCase(), match.title.toLowerCase())) + (match.author_name && match.author_name.length ? levenshtein(book.Author.toLowerCase(), match.author_name.join(", ").toLowerCase()) : 5);
-		const percent = matchPercent(book.Title, match.title, book.Author, match.author_name);
-		console.log(percent);
-		if (percent > highest && percent >= lowestPercent) {
-			highest = percent;
-			bestmatch = match;
-		}
-	}
-	if (bestmatch) {
-		bestmatch.percent = highest;
-		bestmatch.authors = bestmatch.author_name;
-	}
-	return bestmatch;
-}
-
-function findBestGoogleBooksMatch(book, matches) {
-	let bestmatch;
-	let highest = 0;
-	for (let match of matches) {
-		if (book.ISBN && match.industryIdentifiers && match.industryIdentifiers.map(id => id.identifier).includes(book.ISBN)) {
-			return match;
-		}
-		/*if (match.publisher && match.publish_year && match.publisher.includes(book.Publisher) && match.publish_year.includes(book.Date)) {
-			return match;
-		}*/
-		if (match.title) {
-			const percent = matchPercent(book.Title, match.title, book.Author, match.authors);
-			//const lev = Math.min(levenshtein(book.Title.split(":")[0].toLowerCase(), match.title.toLowerCase()), levenshtein(book.Title.toLowerCase(), match.title.toLowerCase())) + (match.authors && match.authors.length ? levenshtein(book.Author.toLowerCase(), match.authors.join(", ").toLowerCase()) : 5);
-			if (percent > highest && percent >= lowestPercent) {
-				lowest = percent;
-				bestmatch = match;
-			}
-			}
-	}
-	if (bestmatch) bestmatch.percent = highest;
-	return bestmatch;
+	return {...bestmatch, percent: highest};
 }
 
 function printResult(book, match) {
@@ -215,46 +237,42 @@ function printResult(book, match) {
 	console.log(match.title, "=>", match.authors, match.percent);
 }
 
-const download = false;
-const lowestPercent = 90;
+const download = true;
+const refresh = false;
+const lowestPercent = 80;
 
 async function all() {
 	let books = await getBooks();
+	books = books.filter((book) => book.Title);
 	let total = 0;
+	let matched = 0;
+	const matchedBooks = [];
 	for (let book of books) {
 		book = cleanBook(book);
-		let matches;
+		// console.log(book);
 		let match;
-		matches = await getGoogleBooks(book);
-		if (matches && matches.length) {
-			match = findBestMatch(book, matches.map(match => {
-				return {
-					...match,
-					isbn: (match.industryIdentifiers ? match.industryIdentifiers.map(id => id.identifier) : []),
-				};
-			}));
-			if (match) {
-				total++;
-			} else {
-				matches = await getOpenLibrary(book);
-				if (matches && matches.length) {
-					match = findBestMatch(book, matches.map(match => {
-						return {
-							...match,
-							authors: matches.author_name,
-						};
-					}));
-					if (match) {
-						total++;
-					}
-				}
-			}
-		}
-		if (match && (match.percent < 82)) {
-			//printResult(book, match);
+		const isbn = await getISBN(book);
+		const google = await getGoogleBooks(book);
+		// const open = [];
+		// const open = await getOpenLibrary(book);
+		const matches = [
+			...isbn,
+			...google,
+			// ...open,
+		].filter(match => match.title);
+		if (matches?.length) match = findBestMatch(book, matches);
+		total++;
+		if (match && (match.percent > lowestPercent)) {
+			matchedBooks.push(match);
+			matched++;
+			console.log('Matched:', match.source + ':', book.Title, "|", book.Author, '=>', match.title , '|', match.authors, '   ', match.percent, '%   ', matched, '/', total);
+		} else if (match) {
+			console.log('No match:', match.source + ':', book.Title, "|", book.Author, '=>', match.title , '|', match.authors, '   ', match.percent, '%   ', matched, '/', total);
+		} else {
+			console.log('No match:', book.Title, "|", book.Author, '   ', 0, '%   ', matched, '/', total);
 		}
 	}
-	console.log(total);
+	fs.writeFileSync('Books.json', JSON.stringify(matchedBooks, null, '\t'));
 }
 
 all();
